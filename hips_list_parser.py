313bc1e3273d78220d0d5f61bc75d3fa2adf05f4
Peter Williams <peter@newton.cx>
2021-12-16 10:03:55 -0500

    hips_list_parser.py: reformat with "black" and add copyright header

--- hips_list_parser.py
+++ hips_list_parser.py
@@ -1,29 +1,62 @@
+#! /usr/bin/env python
+#
+# Copyright 2021 the .NET Foundation
+# Licensed under the MIT License
+
 from xml.etree.ElementTree import Element
 import xml.etree.ElementTree as et
 import requests
 
 PLANETS_CATEGORY_NAME = "Planets & Moons"
 
-class Convert:
 
+class Convert:
     def __init__(self):
         self.root = et.Element("Folder", self.get_default_attributes("HIPS Surveys"))
-        self.images = et.SubElement(self.root, "Folder", self.get_default_attributes("Images"))
-        self.catalogs = et.SubElement(self.root, "Folder", self.get_default_attributes("Catalogs"))
-        self.heatmaps = et.SubElement(self.root, "Folder", self.get_default_attributes("Heatmaps"))
+        self.images = et.SubElement(
+            self.root, "Folder", self.get_default_attributes("Images")
+        )
+        self.catalogs = et.SubElement(
+            self.root, "Folder", self.get_default_attributes("Catalogs")
+        )
+        self.heatmaps = et.SubElement(
+            self.root, "Folder", self.get_default_attributes("Heatmaps")
+        )
 
-        self.images_gamma = et.SubElement(self.images, "Folder", self.get_default_attributes("Gamma"))
-        self.images_xray = et.SubElement(self.images, "Folder", self.get_default_attributes("XRay"))
-        self.images_uv = et.SubElement(self.images, "Folder", self.get_default_attributes("Ultraviolet"))
-        self.images_visible = et.SubElement(self.images, "Folder", self.get_default_attributes("Visible"))
-        self.images_ir = et.SubElement(self.images, "Folder", self.get_default_attributes("IR"))
-        self.images_microwave = et.SubElement(self.images, "Folder", self.get_default_attributes("Microwave"))
-        self.images_radio = et.SubElement(self.images, "Folder", self.get_default_attributes("Radio"))
-        self.images_planets = et.SubElement(self.images, "Folder", self.get_default_attributes(PLANETS_CATEGORY_NAME))
-        self.images_uncategorized = et.SubElement(self.images, "Folder", self.get_default_attributes("Uncategorized"))
+        self.images_gamma = et.SubElement(
+            self.images, "Folder", self.get_default_attributes("Gamma")
+        )
+        self.images_xray = et.SubElement(
+            self.images, "Folder", self.get_default_attributes("XRay")
+        )
+        self.images_uv = et.SubElement(
+            self.images, "Folder", self.get_default_attributes("Ultraviolet")
+        )
+        self.images_visible = et.SubElement(
+            self.images, "Folder", self.get_default_attributes("Visible")
+        )
+        self.images_ir = et.SubElement(
+            self.images, "Folder", self.get_default_attributes("IR")
+        )
+        self.images_microwave = et.SubElement(
+            self.images, "Folder", self.get_default_attributes("Microwave")
+        )
+        self.images_radio = et.SubElement(
+            self.images, "Folder", self.get_default_attributes("Radio")
+        )
+        self.images_planets = et.SubElement(
+            self.images, "Folder", self.get_default_attributes(PLANETS_CATEGORY_NAME)
+        )
+        self.images_uncategorized = et.SubElement(
+            self.images, "Folder", self.get_default_attributes("Uncategorized")
+        )
 
-        self.heatmaps_by_object = et.SubElement(self.heatmaps, "Folder", self.get_default_attributes("By Object Type"))
-        self.heatmaps_by_date = et.SubElement(self.heatmaps, "Folder", self.get_default_attributes("By Date"))
+        self.heatmaps_by_object = et.SubElement(
+            self.heatmaps, "Folder", self.get_default_attributes("By Object Type")
+        )
+        self.heatmaps_by_date = et.SubElement(
+            self.heatmaps, "Folder", self.get_default_attributes("By Date")
+        )
 
         self.add_version_dependent(self.root, False)
         self.add_version_dependent(self.images, False)
@@ -52,8 +85,15 @@ class Convert:
         tree.write(out_file, encoding="utf-8", xml_declaration=True)
 
     def get_default_attributes(self, name: str):
-        return {"MSRCommunityId": "0", "MSRComponentId": "0", "Permission": "0",
-                "Name": name, "Group": "Explorer", "Searchable": "False", "Type": "Sky"}
+        return {
+            "MSRCommunityId": "0",
+            "MSRComponentId": "0",
+            "Permission": "0",
+            "Name": name,
+            "Group": "Explorer",
+            "Searchable": "False",
+            "Type": "Sky",
+        }
 
     def get_element_attribute(self, element: Element, attribute: str):
         if attribute in element:
@@ -62,30 +102,33 @@ class Convert:
             return ""
 
     def get_credits_url(self, element: Element):
-        credits_url = self.get_element_attribute(element, 'obs_copyright_url')
+        credits_url = self.get_element_attribute(element, "obs_copyright_url")
         if isinstance(credits_url, list):
-            return self.get_element_attribute(element, 'hips_service_url').strip("/") + "/properties"
+            return (
+                self.get_element_attribute(element, "hips_service_url").strip("/")
+                + "/properties"
+            )
         else:
             return credits_url
 
     def get_credits(self, element: Element):
-        credits = self.get_element_attribute(element, 'obs_copyright')
+        credits = self.get_element_attribute(element, "obs_copyright")
         if isinstance(credits, list):
             return ", ".join(credits)
         else:
             return credits
 
     def get_name(self, element: Element):
-        name = self.get_element_attribute(element, 'obs_title')
+        name = self.get_element_attribute(element, "obs_title")
         if not name:
-            name = self.get_element_attribute(element, 'ID')
+            name = self.get_element_attribute(element, "ID")
 
         return name
 
     def get_file_type(self, element: Element):
-        file_formats = self.get_element_attribute(element, 'hips_tile_format')
+        file_formats = self.get_element_attribute(element, "hips_tile_format")
 
-        file_formats_arr = file_formats.split(' ')
+        file_formats_arr = file_formats.split(" ")
 
         if "fits" in file_formats_arr and len(file_formats_arr) > 1:
             file_formats_arr.remove("fits")
@@ -95,7 +138,7 @@ class Convert:
         return file_formats
 
     def get_bandpass_name(self, element: Element):
-        regime = self.get_element_attribute(element, 'obs_regime')
+        regime = self.get_element_attribute(element, "obs_regime")
 
         if isinstance(regime, list):
             regime = regime[0]
@@ -117,8 +160,12 @@ class Convert:
         elif "millimeter" in regime or "microwave" in regime:
             return "Microwave"
         else:
-            hips_frame = self.get_element_attribute(element, 'hips_frame').lower()
-            if "galactic" in hips_frame or "ecliptic" in hips_frame or "equatorial" in hips_frame:
+            hips_frame = self.get_element_attribute(element, "hips_frame").lower()
+            if (
+                "galactic" in hips_frame
+                or "ecliptic" in hips_frame
+                or "equatorial" in hips_frame
+            ):
                 return "Uncategorized"
             else:
                 return PLANETS_CATEGORY_NAME
@@ -145,7 +192,7 @@ class Convert:
         self.add_image_set(element, self.catalogs)
 
     def add_image(self, element: Element):
-        regime = self.get_element_attribute(element, 'obs_regime')
+        regime = self.get_element_attribute(element, "obs_regime")
 
         if isinstance(regime, list):
             regime = regime[0]
@@ -167,8 +214,12 @@ class Convert:
         elif "millimeter" in regime or "microwave" in regime:
             self.add_image_set(element, self.images_microwave)
         else:
-            hips_frame = self.get_element_attribute(element, 'hips_frame').lower()
-            if "galactic" in hips_frame or "ecliptic" in hips_frame or "equatorial" in hips_frame:
+            hips_frame = self.get_element_attribute(element, "hips_frame").lower()
+            if (
+                "galactic" in hips_frame
+                or "ecliptic" in hips_frame
+                or "equatorial" in hips_frame
+            ):
                 self.add_image_set(element, self.images_uncategorized)
             else:
                 self.add_image_set(element, self.images_planets)
@@ -189,28 +240,55 @@ class Convert:
 
         dataset_type = "Sky"
         if self.get_bandpass_name(element) == PLANETS_CATEGORY_NAME:
-            if "panorama" in self.get_element_attribute(element, 'obs_title').lower():
+            if "panorama" in self.get_element_attribute(element, "obs_title").lower():
                 dataset_type = "Panorama"
             else:
                 dataset_type = "Planet"
 
-        image_set = et.SubElement(parent, "ImageSet", DemUrl="", MSRCommunityId="0", MSRComponentId="0", Permission="0",
-                                  Generic="False", DataSetType=dataset_type, BandPass=bandpass_name,
-                                  Url=self.get_element_attribute(element, 'hips_service_url').strip("/") + "/Norder{0}/Dir{1}/Npix{2}",
-                                  TileLevels=self.get_element_attribute(element, 'hips_order'), WidthFactor="1",
-                                  Sparse="False", Rotation="0", QuadTreeMap="0123", Projection="Healpix",
-                                  Name=self.get_name(element),
-                                  FileType=self.get_file_type(element), CenterY="0",
-                                  CenterX="0", BottomsUp="False", StockSet="False", ElevationModel="False", OffsetX="0",
-                                  OffsetY="0", BaseTileLevel="0", BaseDegreesPerTile="180", ReferenceFrame="Sky",
-                                  MeanRadius="1")
+        image_set = et.SubElement(
+            parent,
+            "ImageSet",
+            DemUrl="",
+            MSRCommunityId="0",
+            MSRComponentId="0",
+            Permission="0",
+            Generic="False",
+            DataSetType=dataset_type,
+            BandPass=bandpass_name,
+            Url=self.get_element_attribute(element, "hips_service_url").strip("/")
+            + "/Norder{0}/Dir{1}/Npix{2}",
+            TileLevels=self.get_element_attribute(element, "hips_order"),
+            WidthFactor="1",
+            Sparse="False",
+            Rotation="0",
+            QuadTreeMap="0123",
+            Projection="Healpix",
+            Name=self.get_name(element),
+            FileType=self.get_file_type(element),
+            CenterY="0",
+            CenterX="0",
+            BottomsUp="False",
+            StockSet="False",
+            ElevationModel="False",
+            OffsetX="0",
+            OffsetY="0",
+            BaseTileLevel="0",
+            BaseDegreesPerTile="180",
+            ReferenceFrame="Sky",
+            MeanRadius="1",
+        )
 
         et.SubElement(image_set, "Credits").text = self.get_credits(element)
         et.SubElement(image_set, "CreditsUrl").text = self.get_credits_url(element)
-        et.SubElement(image_set, "ThumbnailUrl").text = self.get_element_attribute(element, 'hips_service_url').strip("/") + "/preview.jpg"
+        et.SubElement(image_set, "ThumbnailUrl").text = (
+            self.get_element_attribute(element, "hips_service_url").strip("/")
+            + "/preview.jpg"
+        )
 
 
 conv = Convert()
 
-hips_list_json = requests.get("http://aladin.u-strasbg.fr/hips/globalhipslist?fmt=json").json()
+hips_list_json = requests.get(
+    "http://aladin.u-strasbg.fr/hips/globalhipslist?fmt=json"
+).json()
 conv.convert(hips_list_json).write_file("hips-list.wtml")
